3cf6970ca0f62d1b2d36b2fd5486178fd6b0fe55
cassie365 <cassie@colvindesign.com>
2022-06-03 14:46:15 -0400

    initial commit - added project files

---
+++ src/main/java/com/colvindesign/powerscape/PowerscapeApplication.java
@@ -0,0 +1,13 @@
+package com.colvindesign.powerscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerscapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colvindesign/powerscape/PowerscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colvindesign.powerscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
